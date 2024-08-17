3a735e623d139158c52e0420b8e767ea7687722e
Madhu1107 <145745628+Madhu1107@users.noreply.github.com>
2024-08-17 15:28:51 +1000

    updated dockerfile, build.gradle files
    updated dockerfile, build.gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.24"
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("plugin.jpa") version "1.9.24"
+	//kotlin("plugin.jpa") version "1.9.24"
 
 }
 val kotlin_version = "1.8.22"
@@ -12,16 +12,17 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(18)
+		languageVersion.set(JavaLanguageVersion.of(17))
 	}
 }
+
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 //	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
